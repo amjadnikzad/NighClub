2e043cc021e33ad5eae5122b5d78ecebc9c91c50
4mj4d <amjadnikzad@gmail.com>
2024-07-08 03:17:26 +0330

    First Commit

---
+++ server/src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const logger = new Logger('Main (main.ts)');
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [
+        'http://localhost:8080',
+        /^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):8080$/,
+      ],
+    },
+  });
+  const configService = app.get(ConfigService);
+  const clientPort = parseInt(configService.get('CLIENT_PORT'));
+  app.enableCors(
+    {
+      origin: [
+        `http://localhost:${clientPort}`,
+        new RegExp(`/^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):${clientPort}$/`),
+      ],
+    }
+  )
+  const port = parseInt(configService.get('PORT'));
+  await app.listen(port);
+
+  logger.log(`Server running on port ${port}`);
+}
+bootstrap();
