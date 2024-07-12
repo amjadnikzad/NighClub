3b842fec2965084595c587118ca1c893c3ee1b1a
4mj4d <amjadnikzad@gmail.com>
2024-07-12 12:47:07 +0330

    Added connection and Disconnection functionality to the WebSocketGateway

--- server/src/Games/games.gateway.ts
+++ server/src/Games/games.gateway.ts
@@ -1,18 +1,39 @@
-import { Logger } from "@nestjs/common";
-import { OnGatewayInit, WebSocketGateway } from "@nestjs/websockets";
-import { GamesService } from "./games.service";
-
-
+import { Logger } from '@nestjs/common';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { GamesService } from './games.service';
+import { Namespace, Socket } from 'socket.io';
 
 @WebSocketGateway({
-    namespace:'games'
+  namespace:'games',
 })
-export class GamesGateway implements OnGatewayInit {
-    private readonly logger = new Logger(GamesGateway.name);
-    constructor(private readonly gamesService:GamesService){};
+export class GamesGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private readonly logger = new Logger(GamesGateway.name);
+  constructor(private readonly gamesService: GamesService) {}
+ @WebSocketServer() io:Namespace;
 
-    afterInit(server: any): void {
-        this.logger.log('WebSocket Gateway Intialized')
-        
-    }
+  afterInit(server: any): void {
+    this.logger.log('WebSocket Gateway Intialized');
+  }
+
+  handleConnection(client: Socket) {
+    const sockets = this.io.sockets;
+
+    this.logger.log(`A client WIth ID: ${client.id} has connected`);
+    this.logger.debug(`There Are ${sockets.size} clients connected`);
+  }
+
+  handleDisconnect(client: Socket) {
+    const sockets = this.io.sockets;
+
+    this.logger.log(`A client WIth ID: ${client.id} has Disconnected`);
+    this.logger.debug(`There Are ${sockets.size} clients connected`);
+  }
 }
