2224ca0e8b4f8abe222aeb6788008b63cc242000
4mj4d <amjadnikzad@gmail.com>
2024-10-01 06:06:29 +0330

    Criticial Cors related bug resolved

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,26 +6,20 @@ import { SocketIOAdapter } from './socket-io-adapter';
 
 async function bootstrap() {
   const logger = new Logger('Main (main.ts)');
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: [
-        'http://localhost:8080',
-        /^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):8080$/,
-      ],
-    },
-  });
+  const app = await NestFactory.create(AppModule);
+
   const configService = app.get(ConfigService);
-  const clientPort = parseInt(configService.get('CLIENT_PORT'));
-  app.enableCors(
-    {
-      origin: [
-        `http://localhost:${clientPort}`,
-        new RegExp(`/^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):${clientPort}$/`),
-      ],
-    }
-  );
-  app.useWebSocketAdapter(new SocketIOAdapter(app, configService));
   const port = parseInt(configService.get('PORT'));
+  const clientPort = parseInt(configService.get('CLIENT_PORT'));
+
+  app.enableCors({
+    origin: [
+      `http://localhost:${clientPort}`,
+      new RegExp(`/^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):${clientPort}$/`),
+    ],
+  });
+  app.useWebSocketAdapter(new SocketIOAdapter(app, configService));
+
   await app.listen(port);
 
   logger.log(`Server running on port ${port}`);
